03b88bf115f5b04d13d07ca7545dcc3066c234cc
EvertonSilvaReis <everton.dev.itabuna@gmail.com>
2025-12-16 21:42:05 -0300

    ajuste em query

--- src/main/java/com/mrsoftware/MRFinanceiro/modelo/repositorios/TipoPagamentoRepositorio.java
+++ src/main/java/com/mrsoftware/MRFinanceiro/modelo/repositorios/TipoPagamentoRepositorio.java
@@ -16,10 +16,10 @@ public interface TipoPagamentoRepositorio extends JpaRepository<TipoPagamento, U
   @Query(
       value =
           """
-                              select * from tipoPagamento t
-                              where ( cast(?1 as text) is null or lower(t.nome) like concat('%',?1,'%')) and
-                              t.data_exclusao is null
-                              """,
+              select * from tipoPagamento t
+              where ( cast(?1 as text) is null or lower(t.nome) like concat('%', lower(?1),'%')) and
+              t.data_exclusao is null
+              """,
       nativeQuery = true)
   Page<TipoPagamento> retornarListaTiposPagamentoPaginados(String descricao, Pageable paginacao);
 
